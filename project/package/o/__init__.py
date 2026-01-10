67b5a382632607dfdfbfb1bcb16c612d1f4f16ac
João <jl95terceira@gmail.com>
2026-01-10 16:29:02 +0000

    snake case

--- project/package/o/__init__.py
+++ project/package/o/__init__.py
@@ -49,19 +49,19 @@ class Sender(SenderIf[bytes]):
         if len(data) > 0:
             N = (len(data) - 1) // _constants.CONTENT_FRAME_SIZE + 1
             for i in range(N-1):
-                sizeFrame    = _constants.SIZE_FRAME_FOR_FULL_CONTENT_FRAME
-                contentFrame = data[i* _constants.CONTENT_FRAME_SIZE:(1+i)*_constants.CONTENT_FRAME_SIZE]
+                size_frame    = _constants.SIZE_FRAME_FOR_FULL_CONTENT_FRAME
+                content_frame = data[i* _constants.CONTENT_FRAME_SIZE:(1+i)*_constants.CONTENT_FRAME_SIZE]
                 outs.send(_constants.CONTENT_AHEAD_SIGNAL)
-                outs.send(sizeFrame)
-                outs.send(contentFrame)
+                outs.send(size_frame)
+                outs.send(content_frame)
             
-            lastFrameContentSize = len(data) - (N-1)* _constants.CONTENT_FRAME_SIZE
-            lastFrameContentSizeAsBytes = _util.int_to_bytes(lastFrameContentSize)
-            sizeFrame = (_constants.SIZE_FRAME_SIZE - len(lastFrameContentSizeAsBytes))*bytes([0]) + lastFrameContentSizeAsBytes
-            contentFrame = data[len(data)-lastFrameContentSize:] + (_constants.CONTENT_FRAME_SIZE-lastFrameContentSize)*bytes([0])
+            last_frame_content_size = len(data) - (N-1)* _constants.CONTENT_FRAME_SIZE
+            last_frame_content_size_as_bytes = _util.int_to_bytes(last_frame_content_size)
+            size_frame = (_constants.SIZE_FRAME_SIZE - len(last_frame_content_size_as_bytes))*bytes([0]) + last_frame_content_size_as_bytes
+            content_frame = data[len(data)-last_frame_content_size:] + (_constants.CONTENT_FRAME_SIZE-last_frame_content_size)*bytes([0])
             outs.send(_constants.CONTENT_AHEAD_SIGNAL)
-            outs.send(sizeFrame)
-            outs.send(contentFrame)
+            outs.send(size_frame)
+            outs.send(content_frame)
             outs.send(_constants.NO_CONTENT_SIGNAL)
         
         else:
