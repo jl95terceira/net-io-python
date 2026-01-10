8148ab900ad84489ede2964391983b398054fb85
João <jl95terceira@gmail.com>
2026-01-10 16:33:32 +0000

    int to bytes

--- project/package/util.py
+++ project/package/util.py
@@ -1,7 +1,12 @@
 from collections import deque as _deque
 
+class NegativeNumberNotConvertibleToBytes(Exception): pass
+
+_ZERO_AS_BYTES = bytes([0])
 def int_to_bytes(x:int):
 
+    if x < 0: raise NegativeNumberNotConvertibleToBytes()
+    elif x == 0: return _ZERO_AS_BYTES
     bytes_list = _deque()
     n = x
     while n > 0:
